d9561d3ed8b8edac07147df81ddcf1d5b98444a2
Rishu Soni <rishusoni6393@gmail.com>
2025-08-25 21:18:39 +0530

    Push all files to make it updated

--- To-Do/ToDo.js
+++ To-Do/ToDo.js
@@ -1,37 +1,78 @@
+let inputCount = 0;
 let taskCount = 0;
-let tasks = document.querySelectorAll("task");
-tasks.forEach(() => {
-    taskCount++;
-});
+let storedTask = null;
 
 // to add at least one task on page load
+for (let i = 1; i <= localStorage.length; i++) {
+
+    storedTask = localStorage.getItem(localStorage.key(i - 1));
+    if (!storedTask) continue;
+    let list = document.createElement("li");
+    list.className = `task${i}`;
+    storedTask = storedTask.slice(storedTask.indexOf(`:`));
+    console.log(storedTask)
+    list.innerHTML = `<p>Task ${i}` + storedTask;
+    document.querySelector("ul").appendChild(list);
+    taskCount =localStorage.length;
+}
 Add();
+
+function Remove() {
+    localStorage.clear();
+    document.querySelector("ul").innerHTML = "";
+    inputCount = 0;
+    Add();
+    console.log("All tasks cleared");
+}
 
 //to add a input field to add a new task
 function Add() {
-
     let newTask = document.createElement("li");
-    newTask.className = `input task${taskCount}`;
-    newTask.innerHTML = `<input type="text" placeholder="New Task is..." autofocus autocorrect="on"><button type="button" class="done" onclick="Done('task${taskCount}')"><img src="correct.svg" alt="" class="sidebuttons" ></button><button type="button" class="cancle" onclick="Cancle('task${taskCount}')"><img src="cross.svg" alt="" class="sidebuttons" ></button>`;
+    newTask.className = `input`;
+    newTask.innerHTML = `<input type="text" placeholder="New Task is..." autofocus autocorrect="on"><button type="button" class="done"><img src="correct.svg" alt="" class="sidebuttons" ></button><button type="button" class="cancle"><img src="cross.svg" alt="" class="sidebuttons" ></button>`;
     document.querySelector("ul").appendChild(newTask);
 }
-// to save the task and remove the input field
-function Done(taskClass) {
+
+addEventListener("click", (event) => {
+    let newTask;
+
+    if (event.target.classList.contains("cancle")) {
+        event.target.parentElement.remove();
+    }
+    else if (event.target.parentElement.classList.contains("cancle")) {
+        event.target.parentElement.parentElement.remove();
+    }
+
+    else if (event.target.classList.contains("buttonDel")) {
+        event.target.parentElement.remove();
+        let keytoremove = event.target.parentElement.classList;
+        localStorage.removeItem(keytoremove);
+    }
+    else if (event.target.parentElement.classList.contains("buttonDel")) {
+        event.target.parentElement.parentElement.remove();
+        let keytoremove = event.target.parentElement.parentElement.classList;
+        localStorage.removeItem(keytoremove);
+    }
+
+    else if (event.target.classList.contains("done")) {
+        newTask = event.target.parentElement;
+    }
+    else if (event.target.parentElement.classList.contains("done")) {
+        newTask = event.target.parentElement.parentElement;
+    }
+    if (!newTask) return; {
         taskCount++;
-    let newTask = document.querySelector(`.${taskClass}`);
-    newTask.className = `task task${taskCount}`;
-    let textInput = newTask.querySelector("input").value;
-    newTask.innerHTML = `<p>Task ${taskCount}: ${textInput}</p> <button type="button" class="buttonDel"><img src="TrashIcon.svg" alt="Delete Task" onclick="Delete('task${taskCount}')" ></button>`;
-    document.querySelector("ul").appendChild(newTask);
-}
+        newTask.className = `task${taskCount}`;
+        let textInput = newTask.querySelector("input").value;
+        newTask.innerHTML = `<p>Task ${taskCount}: ${textInput}</p> <button type="button" class="buttonDel"><img src="TrashIcon.svg" alt="Delete Task"  ></button>`;
+        localStorage.setItem(`task${taskCount}`, `<p>Task ${taskCount}: ${textInput}</p> <button type="button" class="buttonDel"><img src="TrashIcon.svg" alt="Delete Task"  ></button>`);
+        document.querySelector("ul").appendChild(newTask);
+    }
 
-// to remove the input field without saving    
-function Cancle(taskClass) {
-    taskCount--;
-    document.querySelector(`.${taskClass}`).remove();
-}
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
+        console.log(key, value);
+    }
 
-// to delete the task
-function Delete(taskClass) {
-    document.querySelector(`.${taskClass}`).remove();
-}
+});
